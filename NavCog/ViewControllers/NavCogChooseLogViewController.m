3435591e1fca8fc48ef7362dccecef3a707155e9
Dragan Ahmetovic <dragan1@cmu.edu>
2015-12-04 09:38:07 -0500

    rebase 2: chooselogviewcontroller

---
+++ NavCog/ViewControllers/NavCogChooseLogViewController.m
@@ -0,0 +1,9 @@
+//
+//  NavCogChooseLogViewController.m
+//  NavCog
+//
+//  Created by navcog on 12/4/15.
+//  Copyright © 2015 Chengxiong Ruan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
